5831cf9f7a3edd7b5d55a4edfe13c510785ef645
Marcin <maethi@gmail.com>
2022-05-05 21:00:44 +0200

    Od zera ogarnięcie flexa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SocialiconsComponent } from './socialicons/socialicons.component';
-import { AboutmeComponent } from './aboutme/aboutme.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SocialiconsComponent,
-    AboutmeComponent,
   ],
   imports: [
     BrowserModule
